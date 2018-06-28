246485db6b37626ce2edf31831d6a9b942f567a9
len-r <ahmedeev.lenar@yandex.ru>
2018-06-28 19:19:14 +0400

    +dbcontext

---
+++ SoBesedka/SoBesedkaDB/SoBesedkaDBContext.cs
@@ -0,0 +1,29 @@
+﻿
+using SoBesedkaModels;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+
+
+namespace SoBesedkaDB
+{
+    public class SoBesedkaDBContext : DbContext
+    {
+        public SoBesedkaDBContext() : base("SoBesedkaDBTable")
+        {
+            Configuration.ProxyCreationEnabled = false;
+            Configuration.LazyLoadingEnabled = false;
+            var ensureDLLIsCopied = System.Data.Entity.SqlServer.SqlProviderServices.Instance;
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+
+        public virtual DbSet<Meeting> Meetings { get; set; }
+
+        public virtual DbSet<Room> Rooms { get; set; }
+
+
+        public virtual DbSet<UserMeeting> UserMeetings { get; set; }
+
+    }
+}
