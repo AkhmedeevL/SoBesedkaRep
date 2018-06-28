a89fbac9e915a2c75bd877f9f3eb35cd2dd902d4
alefly <alyona.krebs@yandex.ru>
2018-06-28 20:25:18 +0400

    fixed all

--- SoBesedka/SoBesedkaServer/Global.asax.cs
+++ SoBesedka/SoBesedkaServer/Global.asax.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
 
-namespace CarFactoryApiRest
+namespace SoBesedkaRestAPI
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
