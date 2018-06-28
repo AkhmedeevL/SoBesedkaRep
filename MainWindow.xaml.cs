a3dadd2024859a0639e2dc402135ab9baa79b98f
Олег Г <skfounder1@gmail.com>
2018-06-29 02:07:30 +0400

    branch madness

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+using System.Windows.Shapes;
+
+namespace SoBesedkaApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DataSamples Data;
+        
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            Data = new DataSamples();
+            DataContext = Data;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            for(int i = 0; i < 7; i++)
+            {
+                Data.CurrentWeek[i] += TimeSpan.FromDays(7);
+            }
+            Data.RaisePropertyChanged("CurrentWeek");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                Data.CurrentWeek[i] -= TimeSpan.FromDays(7);
+            }
+            Data.RaisePropertyChanged("CurrentWeek");
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            MeetingWindow meetingwindow = new MeetingWindow();
+            meetingwindow.Show();
+
+        }
+      
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBoxResult res = MessageBox.Show("Вы действительно хотите выйти?",
+                                             "Выход",
+                                             MessageBoxButton.YesNo,
+                                             MessageBoxImage.Question);
+            if (res != MessageBoxResult.Yes)
+            {
+                    e.Cancel = true;
+            }
+
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ RegWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Sql;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SoBesedkaApp
+{
+    /// <summary>
+    /// Логика взаимодействия для RegWindow.xaml
+    /// </summary>
+    public partial class RegWindow : Window
+    {
+        public RegWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void EnterButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FIOTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            var sndr = (TextBox) sender;
+            
+            Label label = new Label
+            {
+                Content = sndr.Text,
+                FontSize = sndr.FontSize,
+                Margin = new Thickness(sndr.Margin.Left - 2, sndr.Margin.Top - 4, sndr.Margin.Right, sndr.Margin.Bottom),
+                Height = sndr.Height,
+                Width = sndr.Width,
+                HorizontalAlignment = sndr.HorizontalAlignment,
+                VerticalAlignment = sndr.VerticalAlignment
+            };
+            sndr.Text = String.Empty;
+            sndr.Tag = label;
+            RegGrid.Children.Add(label);
+
+            var a = new ThicknessAnimation()
+            {
+                From = label.Margin,
+                To = new Thickness(label.Margin.Left, label.Margin.Top - 25, label.Margin.Right, label.Margin.Bottom),
+                Duration = TimeSpan.FromSeconds(0.2)
+            };
+            label.BeginAnimation(MarginProperty, a);
+        }
+
+        private void FIOTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            var sndr = (TextBox) sender;
+            var label = (Label)sndr.Tag;
+
+            var a = new ThicknessAnimation
+            {
+                From = label.Margin,
+                To = new Thickness(label.Margin.Left, label.Margin.Top + 25, label.Margin.Right, label.Margin.Bottom),
+                Duration = TimeSpan.FromSeconds(0.2)
+            };
+            a.Completed += anim_Completed;
+            label.BeginAnimation(MarginProperty, a);
+            //
+        }
+
+        private void anim_Completed(object sender, EventArgs e)
+        {
+
+            //RegGrid.Children.Remove((Label)sndr.Tag);
+        }
+    }
+}
