f3c8176827f68fe6682698b4f5d7c6a5b128e65b
alefly <alyona.krebs@yandex.ru>
2018-06-30 03:26:17 +0400

    оповещения

--- SoBesedka/SoBesedkaDB/Implementations/UserService.cs
+++ SoBesedka/SoBesedkaDB/Implementations/UserService.cs
@@ -3,7 +3,10 @@ using SoBesedkaDB.Views;
 using SoBesedkaModels;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -144,6 +147,39 @@ namespace SoBesedkaDB.Implementations
             }
             outUser = null;
             return false;
+        }
+
+        public void SendEmail(string mailAddress, string subject, string text)
+        {
+            MailMessage objMailMessage = new MailMessage();
+            SmtpClient objSmtpClient = null;
+
+            try
+            {
+                objMailMessage.From = new MailAddress("sobesedkaapp@yandex.ru");
+                objMailMessage.To.Add(new MailAddress(mailAddress));
+                objMailMessage.Subject = subject;
+                objMailMessage.Body = text;
+                objMailMessage.SubjectEncoding = System.Text.Encoding.UTF8;
+                objMailMessage.BodyEncoding = System.Text.Encoding.UTF8;
+
+                objSmtpClient = new SmtpClient("smtp.yandex.ru", 587);
+                objSmtpClient.UseDefaultCredentials = false;
+                objSmtpClient.EnableSsl = true;
+                objSmtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+                objSmtpClient.Credentials = new NetworkCredential("sobesedkaapp@yandex.ru","teamb123");
+
+                objSmtpClient.Send(objMailMessage);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                objMailMessage = null;
+                objSmtpClient = null;
+            }
         }
     }
 }
