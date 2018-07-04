a9ce3af1e7df921686fe8a0e8ce9968a6c32be76
len-r <ahmedeev.lenar@yandex.ru>
2018-07-04 22:39:53 +0400

    пофиксил эмейл при регистрации

--- SoBesedka/SoBesedkaApp/RegWindow.xaml.cs
+++ SoBesedka/SoBesedkaApp/RegWindow.xaml.cs
@@ -66,9 +66,10 @@ namespace SoBesedkaApp
                 return;
             }
             string mail = EmailTextBox.Text;
-            if (!string.IsNullOrEmpty(mail))
+            string checkmail = mail.ToLower();
+            if (!string.IsNullOrEmpty(checkmail))
             {
-                if (!Regex.IsMatch(mail, @"^(?("")(""[^""]+?""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                if (!Regex.IsMatch(checkmail, @"^(?("")(""[^""]+?""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
                 @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9]{2,17}))$"))
                 {
                     ErrorText.Text = "Неверный формат электронной почты";
