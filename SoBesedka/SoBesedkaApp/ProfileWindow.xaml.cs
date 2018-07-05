b2d157739fdc7b87a7da354e778098e548868334
len-r <ahmedeev.lenar@yandex.ru>
2018-07-05 23:36:57 +0400

    поправил сообщение об успешном обновлении данных при редактировании данных пользователя

--- SoBesedka/SoBesedkaApp/ProfileWindow.xaml.cs
+++ SoBesedka/SoBesedkaApp/ProfileWindow.xaml.cs
@@ -122,7 +122,6 @@ namespace SoBesedkaApp
             }
             else
             {
-                string changed = ""; //выведем пользователю те поля, которые были изменены. изменяются только те поля, которые он ввёл в соответствующие текстбоксы
                 var user = Data.CurrentUser;
 
 
@@ -132,7 +131,6 @@ namespace SoBesedkaApp
                     if (PasswordTextBox.Password == PasswordConfirmTextBox.Password)
                     {
                         user.UserPassword = Data.GetHashString(PasswordTextBox.Password);
-                        changed += " Пароль; ";
                     }
                     else
                     {
@@ -147,17 +145,14 @@ namespace SoBesedkaApp
                 if (FIOTextBox.Text != CurrentFIO)
                 {
                     user.UserFIO = FIOTextBox.Text;
-                    changed += " ФИО; ";
                 }
                 if (LoginTextBox.Text != CurrentLogin)
                 {
                     user.UserLogin = LoginTextBox.Text;
-                    changed += " Логин; ";
                 }
                 if (EmailTextBox.Text != CurrentEmail)
                 {
                     user.UserMail = EmailTextBox.Text;
-                    changed += " E-mail; ";
                 }
                 try
                 {
@@ -172,7 +167,7 @@ namespace SoBesedkaApp
                 {
                     MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
-                MessageBox.Show(changed + " были обновлены", "Успешно изменено", MessageBoxButton.OK);
+                MessageBox.Show("Данные были обновлены", "Успешно изменено", MessageBoxButton.OK);
 
                 ChangeProfileButton.Content = "Редактировать";
                 FIOTextBox.Focusable = false;
